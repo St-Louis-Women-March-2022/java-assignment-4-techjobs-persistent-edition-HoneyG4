3057f6c4b31399960c9a97d94f36972f3f8e21ba
Gabrielle <gabrielle.renee1004@gmail.com>
2022-11-26 19:45:20 -0600

    Nearly completed Part 1 and Part 2, workingf on submitting forms

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 -- Part 1: Test it with SQL
-
+--Select all:
+SELECT name FROM employer
+ WHERE location = "St. Louis City";
 -- Part 2: Test it with SQL
 
 -- Part 3: Test it with SQL
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,34 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+
 public class Employer extends AbstractEntity {
 
+    @NotNull
+    @Size(max =50)
+    private String location;
 
-}
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer() {
+    }
+
+
+
+    }
+
